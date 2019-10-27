50bc9bd61315c67272c2ab1e092652fd5b5d2a11
Eyal Ron <eyal670@gmail.com>
2019-10-27 12:07:54 +0200

    not running on multiple forms in page

--- js/divine_validate.js
+++ js/divine_validate.js
@@ -9,46 +9,48 @@
 jQuery(document).ready(function () {
     // Initialize form validation on the registration form.
     // It has the name attribute "registration"
-    jQuery(".divine_validation form").validate({
-        // Specify validation rules
-        rules: {
-            debug: false,
-            // The key name on the left side is the name attribute
-            // of an input field. Validation rules are defined
-            // on the right side
-            "form_fields[client_num]": {
-                required: true,
-                minlength: 9,
-                maxlength: 9,
-                num_start: true
+    jQuery(".divine_validation form").each(function () {
+        jQuery(this).validate({
+            // Specify validation rules
+            rules: {
+                debug: false,
+                // The key name on the left side is the name attribute
+                // of an input field. Validation rules are defined
+                // on the right side
+                "form_fields[client_num]": {
+                    required: true,
+                    minlength: 9,
+                    maxlength: 9,
+                    num_start: true
+                },
+                "form_fields[phone]": {
+                    required: true,
+                    minlength: 10,
+                    maxlength: 10,
+                    phone_start: true
+                }
             },
-            "form_fields[phone]": {
-                required: true,
-                minlength: 10,
-                maxlength: 10,
-                phone_start: true
-            }
-        },
-        // Specify validation error messages
-        messages: {
-            "form_fields[client_num]": {
-                required: "נא הכנס מספר לקוח",
-                minlength: "מספר לקוח לא תקין - יש להזין מספר בן 9 ספרות",
-                maxlength: "מספר לקוח לא תקין - יש להזין מספר בן 9 ספרות",
-                num_start: "מספר לקוח חייב להתחיל באחד מהצרופים הבאים: 411, 96, 95"
+            // Specify validation error messages
+            messages: {
+                "form_fields[client_num]": {
+                    required: "נא הכנס מספר לקוח",
+                    minlength: "מספר לקוח לא תקין - יש להזין מספר בן 9 ספרות",
+                    maxlength: "מספר לקוח לא תקין - יש להזין מספר בן 9 ספרות",
+                    num_start: "מספר לקוח חייב להתחיל באחד מהצרופים הבאים: 411, 96, 95"
+                },
+                "form_fields[phone]": {
+                    required: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05",
+                    minlength: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05",
+                    maxlength: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05",
+                    phone_start: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05"
+                }
             },
-            "form_fields[phone]": {
-                required: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05",
-                minlength: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05",
-                maxlength: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05",
-                phone_start: "נא להזין מספר טלפון נייד בעל 10 ספרות המתחיל ב 05"
+            // Make sure the form is submitted to the destination defined
+            // in the "action" attribute of the form when valid
+            submitHandler: function (form) {
+                form.submit();
             }
-        },
-        // Make sure the form is submitted to the destination defined
-        // in the "action" attribute of the form when valid
-        submitHandler: function (form) {
-            form.submit();
-        }
+        });
     });
 });
 
